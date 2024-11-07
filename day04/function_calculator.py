f2a8ae318eeeaa4dcc4816244ec159c0a0ec4285
Vivian Fernandes <vivianjoyson1989@gmail.com>
2024-11-07 17:39:07 +0400

    Added explanation files and new programs

---
+++ day04/function_calculator.py
@@ -0,0 +1,16 @@
+def addition(num1, num2):
+    add = num1 + num2
+    return add
+
+def substraction(num1, num2):
+    sub = num1 - num2
+    return sub
+
+def multiply(num1, num2):
+    mul = num1 * num2
+    return mul
+
+def divide(num1, num2):
+    div = num1 / num2
+    return div
+
---
+++ day04/modules_calc.py
@@ -0,0 +1,9 @@
+import function_calculator as calc  #calling the 1st program here as modules
+
+print(calc.addition(3,4))
+
+print(calc.substraction(9,3))
+
+print(calc.multiply(8,2))
+
+print(calc.divide(8,3))
