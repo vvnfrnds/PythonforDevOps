adb83d9f1f9b8e498ca28922a346eefa39896df3
Vivian Fernandes <vivianjoyson1989@gmail.com>
2024-11-19 12:51:23 +0400

    environment variables files

--- day05/02-cmd-args.py
+++ day05/02-cmd-args.py
@@ -4,11 +4,9 @@ def add(num1,num2):
     a = num1 + num2
     return a
 
-
 def sub(num1,num2):
     s = num1 - num2
     return s
-
 
 def mul(num1,num2):
     m = num1 * num2
@@ -38,6 +36,3 @@ else:
     output = "Invalid Operator"
 
 print(output)
-
-
-
---
+++ day05/03-env-vars.py
@@ -0,0 +1,5 @@
+import os
+
+print(os.getenv("password"))
+
+print(os.getenv("apitoken"))
